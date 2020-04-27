2317fe1ed9801d2f8ff6f058f69dffbbe29a5ed1
Sunil1208 <skmy03@gmail.com>
2020-04-27 11:07:15 +0530

    Models fro category, Users and product created

---
+++ models/product.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 2000
+    },
+    price: {
+        type: Number,
+        required: true,
+        maxlength: 32
+    }
+})
