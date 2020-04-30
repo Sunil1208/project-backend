07370a8c00b2097731a8307934e81c67b2aabcce
Sunil1208 <skmy03@gmail.com>
2020-04-30 17:49:02 +0530

    Bulk write, product stock increment and sold decrement

--- controllers/product.js
+++ controllers/product.js
@@ -157,3 +157,23 @@ exports.getAllProducts = (req, res) => {
             res.json(products)
         })
 }
+
+exports.updateStock = (req, res, next) => {
+    
+    let myOperations = req.body.order.products.map(prod => {
+        return {
+            updateOne: {
+                filter: {_id: prod._id},     //find the product using product id
+                update: {$inc: {stock: -prod.count, sold: +prod.count}}
+            }
+        }
+    })
+    Product.bulkWrite(myOperations, {}, (err, products) => {
+        if(err){
+            return res.status(400).json({
+                error: "Bulk operation failed"
+            })
+        }
+        next()
+    })
+}
